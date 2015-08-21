816b6baeb4c560ac5ebb25c0c72265d2ab10b729
aubravo <alvaroubravo@gmail.com>
2015-08-21 14:08:14 -0500

    Chages to pololu controller

--- src/move_base_triala.cpp
+++ src/move_base_triala.cpp
@@ -1,11 +1,8 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Vector3.h>
+#include <serial/serial.h>
 #include <string>
-#include <fcntl.h>
-#include <termios.h>
-#include <unistd.h>
-#include <sys/stat.h>
 
 // POLOLU SERVO MASTER TAGS
 const uint cmpct_target= 0x84;
@@ -20,8 +17,6 @@ const uint m2_ = 0x01;
 const uint m3_ = 0x02;
 const uint m4_ = 0x03;
 
-const char* device = "/dev/ttyACM0";
-int fd = open(device,O_RDWR | O_NOCTTY);
 
 class MotorControl
 {
@@ -33,11 +28,14 @@ void velCallback(const geometry_msgs::Twist::ConstPtr& Twist);
 void accelCallback(const geometry_msgs::Vector3::ConstPtr& Accel);
 void orientCallback(const geometry_msgs::Vector3::ConstPtr& Orient);
 void Pololu_set_target(int mn_, int trgt);
-//void Pololu_init();
-
+void Pololu_set_speed();
+void Pololu_set_accel();
 
 
 double a_, b_, m1_target, m2_target, m3_target, m4_target, m1_vel, m2_vel, m3_vel, m4_vel;
+
+serial::Serial serial_;
+std::string serial_addr;
 
 //ROS
 ros::NodeHandle nh_;
@@ -48,25 +46,38 @@ ros::Subscriber get_accel;
 ros::Subscriber get_orient;
 };
 
-MotorControl::MotorControl()
+MotorControl::MotorControl():
+serial_addr("/dev/ttyACM1")
 {
 	nh_.param("a",a_,a_);
 	nh_.param("b",b_,b_);
+	nh_.param("serial_port",serial_addr,serial_addr);
 	
-	
-	
-	if(fd == -1)
+	try
 	{
-    perror(device);
- 	}
+		ROS_INFO("setting up address");
+		serial_.setPort(serial_addr);
+		ROS_INFO("setting up baudrate");
+		serial_.setBaudrate(9600);
+		ROS_INFO("setting up Timeout");
+		serial::Timeout to = serial::Timeout::simpleTimeout(1000);
+		serial_.setTimeout(to);
+		ROS_INFO("opening port");
+		serial_.open();
+	}
+	catch(serial::IOException& e)
+	{
+		ROS_INFO("UNABLE TO OPEN PORT");
+	}
 
-	struct termios options;
-	tcgetattr(fd, &options);
-	options.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
-	options.c_oflag &= ~(ONLCR | OCRNL);
-	tcsetattr(fd, TCSANOW, &options);
-
-	//Pololu_init();
+	if(serial_.isOpen()){
+		ROS_INFO("SERIAL PORT INITIALIZED");
+		Pololu_set_target(m1_,127);
+		Pololu_set_target(m2_,127);
+		Pololu_set_target(m3_,127);
+		Pololu_set_target(m4_,127);
+		Pololu_set_speed();
+	}
 
 	get_vel = nh_.subscribe<geometry_msgs::Twist>("cmd_vel", 10, &MotorControl::velCallback, this);
 	get_accel = nh_.subscribe<geometry_msgs::Vector3>("sensor_accel", 10, &MotorControl::accelCallback, this);
@@ -75,34 +86,19 @@ MotorControl::MotorControl()
 
 void MotorControl::Pololu_set_target(int mn_, int trgt){
 	char wr[] = {mini_target,mn_,trgt};
-	if(write(fd,wr,sizeof(wr)) == -1){
-		ROS_ERROR("ERROR WRITING POSITION");
-	}
+	serial_.write(wr);
 }
 
-/*void MotorControl::Pololu_init(){
-	char wr[] = {cmpct_set_speed,m1_,0x0C,0x01,cmpct_set_speed,m2_,0x0C,0x01,cmpct_set_speed,m3_,0x0C,0x01,cmpct_set_speed,m4_,0x0C,0x01,cmpct_set_accel,m1_,0x00,0x00};
-
-	if(write(fd,wr,sizeof(wr)) == -1);
-	{
-		ROS_ERROR("ERROR INITIALIZING DEVICE:SPEED SET");
-	}
-	
-	char wr2[] = {cmpct_set_accel,m2_,0x00,0x00,cmpct_set_accel,m3_,0x00,0x00,cmpct_set_accel,m4_,0x00,0x00};
-
-	if(write(fd,wr2,sizeof(wr2)) == -1);
-	{
-		ROS_ERROR("ERROR INITIALIZING DEVICE:ACCEL SET");
-	}
-
-	char wr3[] = {mini_target,m1_,127,mini_target,m2_,127,mini_target,m3_,127,mini_target,m4_,127};
-
-	if(write(fd,wr3,sizeof(wr3)) == -1);
-	{
-		ROS_ERROR("ERROR INITIALIZING DEVICE:TARGET SET");
-	}
-	
-}*/
+void MotorControl::Pololu_set_speed(){
+	char wr[] = {cmpct_set_speed,m1_,0x0C,0x01,cmpct_set_speed,m2_,0x0C,0x01,cmpct_set_speed,m3_,0x0C,0x01,cmpct_set_speed,m4_,0x0C,0x01};
+	serial_.write(wr);
+	ROS_INFO("CHANGE SPEED SET");
+}
+void MotorControl::Pololu_set_accel(){
+	char wr[] = {cmpct_set_accel,m1_,0x00,0x00,cmpct_set_accel,m2_,0x00,0x00,cmpct_set_accel,m3_,0x00,0x00,cmpct_set_accel,m4_,0x00,0x00};
+	serial_.write(wr);
+	ROS_INFO("CHANGE ACCEL SET");
+}
 
 void MotorControl::velCallback(const geometry_msgs::Twist::ConstPtr& Twist)
 {
@@ -117,7 +113,6 @@ void MotorControl::velCallback(const geometry_msgs::Twist::ConstPtr& Twist)
 	m3_target = 127+127*(-(y_vel - x_vel - w_vel*(a_+b_)));
 	m4_target = 127+127*((y_vel + x_vel + w_vel*(a_+b_)));
 	ROS_INFO("CHANGING TARGET");
-
 	Pololu_set_target(m1_,m1_target);
 	Pololu_set_target(m2_,m2_target);
 	Pololu_set_target(m3_,m3_target);
@@ -137,7 +132,6 @@ void MotorControl::orientCallback(const geometry_msgs::Vector3::ConstPtr& Orient
 }
 
 int main(int argc, char** argv){
-
 	ros::init(argc, argv, "move_base");
 	MotorControl motor_control;
 	ros::spin();
